3485cf61352cf3a9822c0f3764d931eee598270d
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2023-11-23 11:08:13 -0600

    Main Menu
    Main menu was added

---
+++ src/Controllers/Main.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+import Views.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
+    }
+}
